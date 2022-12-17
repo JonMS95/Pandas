347e0d93769a22bbe1e84c19c3d3a2b96ab6763f
JonMS95 <jon_portugalete@hotmail.com>
2022-12-17 17:32:17 +0100

    Work in progress...

--- 3_Summary_Functions_and_Maps/main.py
+++ 3_Summary_Functions_and_Maps/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-PATH_WINE_REVS = '/home/jon/Desktop/scripts/ML/Pandas/Data_sets/winemag-data_first150k.csv'
+PATH_WINE_REVS = '/home/jon/Desktop/scripts/ML/Pandas/Data_sets/winemag-data-130k-v2.csv'
 
 reviews = pd.read_csv(PATH_WINE_REVS, index_col = 0)
 
@@ -16,3 +16,10 @@ print(reviews_points_summary)
 # frequencies or counts are displayed in this case.
 reviews_taster_names = reviews.taster_name.describe()
 print(reviews_taster_names)
+
+# Particular values from a summary may be accessed by using their names. Just access them as it's done for a common class attribute.
+print(reviews.points.mean())
+
+# It may happen that some values get repeated along a column. If the aim is to know the names, and not how many times the appear within a column, the
+# unique() method should be used.
+print(reviews.taster_name.unique())
