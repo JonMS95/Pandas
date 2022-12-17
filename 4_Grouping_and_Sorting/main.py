c7ee047803e12ba90b910ab8749892a6aa7457fd
JonMS95 <jon_portugalete@hotmail.com>
2022-12-17 20:05:17 +0100

    WIP

--- 4_Grouping_and_Sorting/main.py
+++ 4_Grouping_and_Sorting/main.py
@@ -4,3 +4,15 @@ PATH_WINE_REVS = '/home/jon/Desktop/scripts/ML/Pandas/Data_sets/winemag-data-130
 
 # Maps allow the user to transform the data within a DataFrame or Series in a specified way. However, sometimes
 # user may want to group the data by following some criteria. This is achieved by using the groupby() function.
+reviews = pd.read_csv(PATH_WINE_REVS, index_col = 0)
+
+# In this case, we are gonna start by separating the data in groups depending on their points. 
+print(reviews.groupby('points').points.count())
+# Basically, what we have done is just to separate them by their points, then counting how many rows have the same
+# value given the grouping criterion.
+
+# Luckily, a shortcut exists to the line shown above: value_counts()
+print(reviews['points'].value_counts())
+
+# If we want to know which is the cheapest wine for each point score:
+print(reviews.groupby('points').price.min())
