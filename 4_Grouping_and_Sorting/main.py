7829054fd705383fc6e45954f8f692542a79cc78
JonMS95 <jon_portugalete@hotmail.com>
2022-12-17 19:36:45 +0100

    WIP

---
+++ 4_Grouping_and_Sorting/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+PATH_WINE_REVS = '/home/jon/Desktop/scripts/ML/Pandas/Data_sets/winemag-data-130k-v2.csv'
+
+# Maps allow the user to transform the data within a DataFrame or Series in a specified way. However, sometimes
+# user may want to group the data by following some criteria. This is achieved by using the groupby() function.
