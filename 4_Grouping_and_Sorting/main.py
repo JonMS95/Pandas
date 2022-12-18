7e6e250dcc7a57aad5b911fc127e1f7d2f759408
JonMS95 <jon_portugalete@hotmail.com>
2022-12-18 17:31:50 +0100

    WIP

--- 4_Grouping_and_Sorting/main.py
+++ 4_Grouping_and_Sorting/main.py
@@ -5,6 +5,7 @@ PATH_WINE_REVS = '/home/jon/Desktop/scripts/ML/Pandas/Data_sets/winemag-data-130
 # Maps allow the user to transform the data within a DataFrame or Series in a specified way. However, sometimes
 # user may want to group the data by following some criteria. This is achieved by using the groupby() function.
 reviews = pd.read_csv(PATH_WINE_REVS, index_col = 0)
+print(reviews.columns)
 
 # In this case, we are gonna start by separating the data in groups depending on their points. 
 print(reviews.groupby('points').points.count())
@@ -16,3 +17,7 @@ print(reviews['points'].value_counts())
 
 # If we want to know which is the cheapest wine for each point score:
 print(reviews.groupby('points').price.min())
+
+# Same as shown in the previous exercise, apply() function (which was used for mapping DataFrames) can be used here
+# as well. For example, if the first element of each group is wanted to be modified:
+reviews.groupby('winery').apply(lambda df : df.title.iloc[0])
